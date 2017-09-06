7e5b17a011072e90737eb58353d9888874e58506
Yash Karle <yashkarle@gmail.com>
2017-09-06 14:27:10 +0530

    Update client.go

--- client.go
+++ client.go
@@ -8,36 +8,35 @@ import "encoding/json"
 
 func main() {
 
-  	// connect to this socket
-  	conn, _ := net.Dial("tcp", "127.0.0.1:8081")
-	ln, _ := net.Listen("tcp", ":8082")
-	conn1, _ := ln.Accept()
-  
-	for { 
-		fmt.Println("After X's turn: \n")
-		var board [][]string
-		dec := json.NewDecoder(conn1)
-		err := dec.Decode(&board)
-		if err != nil {
-			fmt.Println("ERROR:", err)
-		}
-		for i, _ := range board {
-    			fmt.Println(board[i])
-  		}
+	// connect to this socket
+	conn, _ := net.Dial("tcp", "localhost:8081")
 
-    		userInput := bufio.NewReader(os.Stdin)
+	ln, _ := net.Listen("tcp", ":8082")
+
+	for {
+		conn1, _ := ln.Accept()
+
+		var board [][]string
+		json.NewDecoder(conn1).Decode(&board)
+
+		fmt.Println("After X's turn: \n")
+		for i, _ := range board {
+			fmt.Println(board[i])
+		}
+
+		userInput := bufio.NewReader(os.Stdin)
 		// read in input from stdin
-    		fmt.Println("Which row would you like to mark?")
+		fmt.Println("Which row would you like to mark?")
 		row, err := userInput.ReadBytes(byte('\n'))
 		fmt.Println("Which column would you like to mark?")
 		col, err := userInput.ReadBytes(byte('\n'))
 		if err != nil {
 			fmt.Println("ERROR", err)
 		}
-    		// send to socket
-    		conn.Write(row)
+		// send to socket
+		conn.Write(row)
 		conn.Write(col)
- 		//fmt.Fprintf(conn, col + "\n")
-    		// listen for reply
-  	}
+		//fmt.Fprintf(conn, col + "\n")
+		// listen for reply
+	}
 }
